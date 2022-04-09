a9966b76dc3ed5bce4052ae3195955dd3bf8d414
AxelKoerner <91koax1bif@hft-stuttgart.de>
2022-04-09 14:17:23 +0200

    initial commit

---
+++ app/src/main/java/de/hft/pvc/obscurio/ui/settings/SettingsViewModel.kt
@@ -0,0 +1,13 @@
+package de.hft.pvc.obscurio.ui.settings
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SettingsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is settings Fragment"
+    }
+    val text: LiveData<String> = _text
+}
