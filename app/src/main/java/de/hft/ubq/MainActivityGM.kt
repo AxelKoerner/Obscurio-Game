3876d91db4607a9ffec6d593b79fde6b0d645e2a
AxelKoerner <91koax1bif@hft-stuttgart.de>
2022-05-04 22:32:20 +0200

    arrow for gm

--- app/src/main/java/de/hft/ubq/MainActivityGM.kt
+++ app/src/main/java/de/hft/ubq/MainActivityGM.kt
@@ -7,6 +7,7 @@ import android.os.CountDownTimer
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.ubq.R
@@ -26,7 +27,6 @@ class MainActivityGM : AppCompatActivity() {
         mediaPlayer?.start()
         mediaPlayer?.isLooping = true
         //countdown()   <-- crashed die app
-
 
 
 
