93734aeb8f2d0d497cc5d92bfc5379f1df260eaf
Son <91ngtr1bif@hft-stuttgart.de>
2022-05-31 15:35:38 +0200

    midscreen and modus

---
+++ app/src/main/java/de/hft/ubq/Modus.kt
@@ -0,0 +1,12 @@
+package de.hft.ubq
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ubq.R
+
+class Modus : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_modus)
+    }
+}
