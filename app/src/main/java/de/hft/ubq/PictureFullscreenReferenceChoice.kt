bf3175c624aa50d4adccdd0f0fc35390097cbf60
91ngtr1bif <56401370+91ngtr1bif@users.noreply.github.com>
2022-06-15 14:27:50 +0200

    Add files via upload

--- app/src/main/java/de/hft/ubq/PictureFullscreenReferenceChoice.kt
+++ app/src/main/java/de/hft/ubq/PictureFullscreenReferenceChoice.kt
@@ -1,24 +1,35 @@
 package de.hft.ubq
 
+import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.ImageButton
 import android.widget.Switch
 import android.widget.Toast
 import com.example.ubq.R
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class PictureFullscreenReferenceChoice : AppCompatActivity() {
     val shared_Preferences:String = "shared_Preferences"
 
+    private lateinit var database : DatabaseReference
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_picture_fullscreen_reference_choice)
         supportActionBar?.hide()
+
+        database = Firebase.database.reference
 
         fun backtoReferenceChoice() {
             finish()
@@ -121,5 +132,36 @@ class PictureFullscreenReferenceChoice : AppCompatActivity() {
         })
 
 
+    }
+
+    fun getdata(dbchild : String) {
+        database.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                var test = snapshot.child("$dbchild").getValue()
+            }
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w(ContentValues.TAG, "Failed to read value.", error.toException())
+            }
+        })
+    }
+
+    fun setData(dbchild2 : String, saveValue : String, datatype : String) {
+        when(datatype){
+            "Int"->{
+                database.child("$dbchild2").setValue(saveValue.toInt())
+            }
+            "String"->{
+                database.child("$dbchild2").setValue("$saveValue")
+            }
+            "Boolean"->{
+                if (saveValue == "true"){
+                    database.child("$dbchild2").setValue(true)
+                }
+                if (saveValue == "false"){
+                    database.child("$dbchild2").setValue(false)
+                }
+            }
+        }
     }
 }
